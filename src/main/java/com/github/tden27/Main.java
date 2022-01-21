23defee37519dbab2d35bbc93f502a9a040b3ef5
AleksandrSinn <87716139+AleksandrSinn@users.noreply.github.com>
2022-01-21 17:59:08 +0300

    refactoring method generateNextPersonInFullList

--- src/main/java/com/github/tden27/Main.java
+++ src/main/java/com/github/tden27/Main.java
@@ -1,8 +1,31 @@
 package com.github.tden27;
 
+import com.github.tden27.model.Generator;
+import jdk.nashorn.internal.objects.NativeMath;
+
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import static jdk.nashorn.internal.objects.NativeMath.random;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        Map<Integer, String> users = new HashMap<>();
+        users.put(0, "1111");
+        users.put(1, "2222");
+        users.put(2, "3333");
+        users.put(3, "4444");
+        users.put(4, "5555");
+
+        Generator.generateNextPersonInFullList(users);
+        Generator.generateNextPersonInFullList(users);
+        Generator.generateNextPersonInFullList(users);
+        Generator.generateNextPersonInFullList(users);
+        Generator.generateNextPersonInFullList(users);
+        Generator.generateNextPersonInFullList(users);
+
+
     }
 }
--- src/main/java/com/github/tden27/model/Generator.java
+++ src/main/java/com/github/tden27/model/Generator.java
@@ -1,26 +1,57 @@
 package com.github.tden27.model;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import jdk.nashorn.internal.objects.NativeMath;
+
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static jdk.nashorn.internal.objects.NativeMath.random;
+import static jdk.nashorn.internal.runtime.ScriptObject.toPropertyDescriptor;
 
 public class Generator{
-    private static Map<Integer, String> personsWhichICanAsk;
-    private static int personListSize;
+    private static String headPerson;
+    private static String askedPerson;
+    private static List<String> memberNames;
+    private static int callMethodCounter = 0;
 
-    static String generateNextPersonInFullList(Map<Integer, String> personList) {
-        personsWhichICanAsk.putAll(personList);
-        personListSize = personList.size() - 1;
-        Random random = new Random();
-        int localNumber = (int)random(personListSize);
-        String person = personsWhichICanAsk.get(localNumber);
-        personListSize --;
-        personsWhichICanAsk.remove(localNumber);
+    public static void generateNextPersonInFullList(Map<Integer, String> personList) throws ArrayIndexOutOfBoundsException {
+        memberNames = new ArrayList<>(personList.values());
+        if(callMethodCounter == 0 || callMethodCounter == personList.size()){
+            generateFirstPerson();
+            generateAskedPerson();
+            System.out.format("%s, ask %s", headPerson, askedPerson);
+            System.out.println();
+        }
+        callMethodCounter++;
+        headPerson = askedPerson;
+        generateAskedPerson();
+        if(headPerson.equals(askedPerson)){
+            generateAskedPerson();
+        }
+        System.out.format("%s, ask %s", headPerson, askedPerson);
+        System.out.println();
 
-        return person;
     }
 
+    private static void generateFirstPerson() {
+        int number = (int) (Math.random() * (memberNames.size() - 1));
+        headPerson = memberNames.get(number);
+        callMethodCounter = 0;
+    }
+
+    private static void generateAskedPerson() {
+        int number = (int) (Math.random() * (memberNames.size() - 1));
+        askedPerson = memberNames.get(number);
+    }
+
+    private static void removeCash(){
+        headPerson = null;
+        askedPerson = null;
+        callMethodCounter = 0;
+        memberNames = null;
+    }
+
+    public static List<String> getMemberNames() {
+        return memberNames;
+    }
 }
